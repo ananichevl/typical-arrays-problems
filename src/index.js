5c37660ab085379595fc04c9846813ef95569948
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2020-10-02 23:36:59 +0300

    feat: add functions implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if(!(Array.isArray(array) && array.length)) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return min;
+};
+
+exports.max = function max(array) {
+    if(!(Array.isArray(array) && array.length)) {
+        return 0;
+    }
+
+    let max = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if(!(Array.isArray(array) && array.length)) {
+        return 0;
+    }
+
+    let avg = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        avg = avg + array[i];
+    }
+
+    return avg/array.length;
+};
